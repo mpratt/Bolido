8f59a917e2da09c6f260f3b26ee7d27f43499097
Michael Pratt <pratt@hablarmierda.net>
2014-02-07 16:50:35 -0500

    Updated Dependencies/Test files - Removed unused dependencies on composer.json - Updated phpunit.xml and .scrutinizer.yml - Added tests/Autoload.php

---
+++ tests/Autoload.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Autoload.php
+ *
+ * @package Tests
+ * @author Michael Pratt <pratt@hablarmierda.net>
+ * @link   http://www.michael-pratt.com/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
